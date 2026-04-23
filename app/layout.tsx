889438aa4afd41a5ab3124ca343a022eba07a712
Leonid <leonid@example.com>
2026-04-23 16:41:47 +0300

    frontend update - v1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Auto-position',
+  description: 'Auto-position is a next generation positioning service.',
+  generator: 'Auto-position',
   icons: {
     icon: [
       {
